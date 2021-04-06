05cf5faacdb608ba8e66b1bf8bf948df4bcd59c8
muazamhussain7 <muazam.hussain@venturedive.com>
2021-04-06 11:55:12 +0500

    modified title of app in main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Web',
+      title: 'Responsive App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: kPrimaryColor,
